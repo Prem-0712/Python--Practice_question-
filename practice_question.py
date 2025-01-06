c3278baf9002316113f73b17137ce03abcaacb16
spdiey07 <premgangurde0712@gmail.com>
2025-01-06 20:10:23 +0530

    Create practice_question.py

---
+++ practice_question.py
@@ -0,0 +1,54 @@
+# 1. Print Statement 
+# Q1: Print the phrase "Hello, World!" using the print statement. 
+# Q2: Print three different messages on separate lines. 
+# Q3: Print three different messages on separate lines using one print statement.
+# Q4: Concatenate and print two variables
+# Q5: Print three words separated by a specific character ‘$’.
+# Q6: Print two messages on the same line using the end parameter.
+# Q7: Print a message with quotes around it.
+# Q8: Print the result of a Boolean expression.
+
+
+# # Q1: Print the phrase "Hello, World!" using the print statement. 
+
+print ("Hello, World !")
+
+print()
+
+# Q2: Print three different messages on separate lines. 
+
+print("Hello")
+print("Sandip sir")
+print("How are you ?")
+
+print()
+
+# Q3: Print three different messages on separate lines using one print statement.
+print ("Hello \nSandip sir, \nHow are you ?")
+print()
+
+# Q4: Concatenate and print two variables
+print("Hello" + "World")
+
+print()
+
+# .Q5: Print three words separated by a specific character ‘$’.
+
+print("Hello", "World", "Everyone", sep="$")
+
+print()
+
+# Q6: Print two messages on the same line using the end parameter.
+print("Hello", end="")
+print("World", end="")
+print()
+
+# Q7: Print a message with quotes around it.
+
+print("\"Hello world\"")
+
+print()
+
+# Q8: Print the result of a Boolean expression.
+print (5 > 3)
+print(3>5)

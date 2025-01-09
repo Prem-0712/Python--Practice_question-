4aeb62212f710860011b6a3291a7eda10fa55efe
spdiey07 <premgangurde0712@gmail.com>
2025-01-09 19:12:48 +0530

    Create practice_question_5.py

---
+++ practice_question_5.py
@@ -0,0 +1,23 @@
+a = int(input("Enter your number: "))
+i = int(1)
+
+
+while( i <= 10):
+    x = a * i
+    print(x)
+    i += 1
+print("End of table")
+
+print("End of table")
+
+a = int(input("Enter you number: "))
+j = int(1)
+
+while(j <= a):
+    i = int(1)
+    while( i <= 10):
+        print(j, "*", i, "=", j*i)
+        i+=1
+    j += 1
+    print()
+print("End Tables")

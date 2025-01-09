53b5d400da4846be4715fd86e11e4fe70cf436f0
spdiey07 <premgangurde0712@gmail.com>
2025-01-09 20:54:29 +0530

    working as usual

--- practice_question_5.py
+++ practice_question_5.py
@@ -1,23 +1,24 @@
+# a = int(input("Enter you number: "))
+# i = 1
+# while( i <= 10):
+#     print(a, "*", i,"=", a*i)
+#     i += 1
+#     print()
+# print("End of table.")
+# print()
+
 a = int(input("Enter your number: "))
-i = int(1)
+j = 1
 
-
-while( i <= 10):
-    x = a * i
-    print(x)
-    i += 1
-print("End of table")
-
-print("End of table")
-
-a = int(input("Enter you number: "))
-j = int(1)
-
-while(j <= a):
-    i = int(1)
-    while( i <= 10):
-        print(j, "*", i, "=", j*i)
-        i+=1
+while( j <= a):
+    i = 1
+    while(i<=10):
+        print(j, "*", i, "=", j * i)
+        i += 1
+        print()
+    print("End of table")
+    print()
     j += 1
     print()
-print("End Tables")
+print("End of loop")
+print()

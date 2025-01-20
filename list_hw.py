f7efbfc095024e8c74bf5dcf2fbc575becc4005c
spdiey07 <premgangurde0712@gmail.com>
2025-01-20 19:48:20 +0530

    working always

---
+++ list_hw.py
@@ -0,0 +1,14 @@
+# ------------------------this is for integer -----------------
+
+n = int(input("Enter the number of elements in your list: "))
+
+my_list = []
+
+for i in range(n):
+    i = int(input("Enter the elements in your list: "))
+    my_list.append(i)
+
+print(my_list)
+
+# -----------------------------this one is for string-----------------
+

f318b06148c91572aa36c3ee3c7c339d943f09d9
spdiey07 <premgangurde0712@gmail.com>
2025-01-21 17:23:43 +0530

    Update list_hw.py

--- list_hw.py
+++ list_hw.py
@@ -1,14 +1,35 @@
-# ------------------------this is for integer -----------------
+# # ------------------------this is for integer -----------------
 
-n = int(input("Enter the number of elements in your list: "))
+# n = int(input("Enter the number of elements in your list: "))
 
-my_list = []
+# my_list = []
 
-for i in range(n):
-    i = int(input("Enter the elements in your list: "))
-    my_list.append(i)
+# for i in range(n):
+#     i = int(input("Enter the elements in your list: "))
+#     my_list.append(i)
 
-print(my_list)
+# print(my_list)
 
 # -----------------------------this one is for string-----------------
 
+# n = int(input("Enter the number of elements in you list: "))
+
+# my_list = []
+
+# for i in range (n):
+#     i = input("")
+#     my_list.append(i)
+
+# print(my_list)
+
+# -------------------------this one is for float ------------------
+
+# n = int(input("Enter the number of elements in you list: "))
+
+# my_list = []
+
+# for i in range (n):
+#     i = float(input("Enter the elemens in your list: "))
+#     my_list.append(i)
+
+# print(my_list)

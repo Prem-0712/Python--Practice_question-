1f4a0e64005776c87f5576110119ed876a1027cf
spdiey07 <premgangurde0712@gmail.com>
2025-01-25 11:49:29 +0530

    Update factorial_using_for_&_while.py

--- factorial_using_for_&_while.py
+++ factorial_using_for_&_while.py
@@ -1,4 +1,4 @@
-# --TOP TO BOTTOM NUMBERS USING WHILE LOOP---------
+# --BOTTOM TO TOP NUMBERS USING WHILE LOOP---------
 
 # a = int(input("Enter your number: "))
 # i = 0
@@ -7,50 +7,57 @@
 #     i += 1
 
 
-# -----TOP TO BOTTOM USING FOR LOOP-----------
+# -----BOTTOM TO TOP USING FOR LOOP-----------
 
 # a = int(input("Enter your number: "))
 # for i in range(a + 1):
 #     print(i)
 #     i += 1
 
-#---------BOTTOM TO TOP USING WHILE LOOP----------------------------------
+#---------TOP TO BOTTOM USING WHILE LOOP----------------------------------
 
 # a = int(input("Enter your number: "))
 # while (a >= 0):
 #     print(a)
 #     a -= 1
 
-# -------BOTTOM TO TOP USING FOR LOOP----------
+# -------TOP TO BOTTOM USING FOR LOOP----------
 
 # a = int(input("Enter your number: "))
 # for i in range(a, -1, -1):
 #     print(i)
 
-# ----------------------FACTORIAL---------------------------------------------
-
-# a = int(input("Enter your number: "))
-# fact = 1
-# while( a >= 1):
-#     fact = fact * a
-#     a = a - 1
-
-# print(fact)
-
+#-----------FACTORIAL USING WHILE LOOP & BOTTOM TO TOP -------------
 
 # a = int(input("Enter your number: "))
 # i = 1
-# fact = 1 
-
-# while( i <= a ):
+# fact = 1
+# while(i <= a):
 #     fact = fact * i
-#     i = i + 1
+#     i += 1
 # print(fact)
+
+# --------FACTORIAL USING WHILE LOOP & TOP TO BOTTOM--------
+# a = int(input("Enter your number: "))
+# fact = 1
+# while ( a >= 1):
+#     fact = a * fact
+#     a -= 1
+# print(fact)
+
+# -------FACTORIAL USING FOR LOOP & BOTTOM TO TOP-----
 
 # a = int(input("Enter your number: "))
 # fact = 1
-# i = a
-# while( i > 0 ):
-#     fact = fact * i 
-#     i = i - 1
+# for i in range(a):
+#     fact = fact * ( i + 1 )
+# print(fact)
+
+
+# ------FACTORIAL USING FOR LOOP & TOP TO BOTTOM-----
+
+# a = int(input("Enter your number: "))
+# fact = 1
+# for i in range(a, 0, -1):
+#     fact = fact * i
 # print(fact)

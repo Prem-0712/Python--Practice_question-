19bea9b330f64ef4bd08da8a0422f46ef3263a48
spdiey07 <premgangurde0712@gmail.com>
2025-01-25 12:41:01 +0530

    always working

---
+++ armstrong.py
@@ -0,0 +1,64 @@
+# -----ARMSTRONG USING WHILE LOOP-------
+
+# a = int(input("Enter your number: "))
+# original_number = a
+# sum = 0
+# while (a > 0):
+#     x = a % 10
+#     sum += x ** 3
+#     a //= 10
+
+# if ( sum == original_number):
+#     print("It's an ARMSTRONG number.")
+
+# else:
+#     print("It's not an ARMSTRONG number.")
+
+# ------ARMSTRONG NUMBER USING FOR LOOP-----
+
+# a = int(input("Enter your number: "))
+# original_number = a
+# sum = 0
+
+# for i in str(a):
+#     sum = sum + int(i)**3
+
+# if ( sum == original_number):
+#     print("It's an ARMSTRONG NUMBER")
+
+# else:
+#     print("It's not an ARMSTRONG NUMBER !")
+
+# --------------------BIG ARMSTRONG USING FOR LOOP---------------------------------
+
+# a = int(input("Enter your number: "))
+# original_number = a
+# sum = 0
+# counter = 0
+
+# for i in str(a):
+#     counter += 1
+# for j in str(a):
+#     sum += int(j) ** counter
+# if ( sum == original_number):
+#     print("It's an ARMSTRONG NUMBER")
+# else:
+#     print("It's not an ARMSTRONG NUMBER")
+
+# -------------------------ARMSTRONG USING WHILE LOOP---------------------
+
+# a = int(input("Enter your number: "))
+# original_number = a
+# sum = 0
+# i = 0
+# power = len(str(a))
+
+# while ( a > 0 ):
+#     i = a % 10
+#     sum += (i ** power) 
+#     a //= 10
+# if ( sum == original_number):
+#     print("It's an ARMSTRONG NUMBER !!!!")
+
+# else:
+#     print("It's not an ARMSTRONG NUMBER !!")
--- sum_of_digits_using_while_loop.py
+++ sum_of_digits_using_while_loop.py
@@ -1,9 +1,17 @@
-a = int(input("Enter your number: "))
-sum = 0
+# ----SUM OF DIGITS USING WHILE LOOP-------------------
 
-while a > 0:
-    x = a % 10         
-    sum += x      
-    a //= 10  
+# a = int(input("Enter your number: "))
+# sum = 0
+# while ( a > 0):
+#     x = a % 10
+#     sum = sum + x
+#     a = a // 10
+# print(sum)
 
-print(sum)
+# -------SUM OF DIGITS USING FOR LOOP--------------
+
+# a = int(input("Enter your number: "))
+# sum = 0
+# for i in str(a):
+#     sum += int(i)
+# print(sum)

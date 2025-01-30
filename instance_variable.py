8647faebc7ed41240d9788ce6c5e780fd3b18308
spdiey07 <premgangurde0712@gmail.com>
2025-01-30 13:25:09 +0530

    Update instance_variable.py

--- instance_variable.py
+++ instance_variable.py
@@ -1,13 +1,14 @@
 class Car:
+
     def __init__(self, brand, model, year):
         self.brand = brand
         self.model = model
         self.year = year
 
-    def start(self):
-        print(f"The {self.brand} {self.model} of {self.year} is starting. ")
-    
+    def start (self):
+        print (f"The {self.brand} {self.model} of {self.year} is starting !")
 
-ford = Car(brand="Ford", model="Endavour", year="2012")
 
-ford.start()
+Ford = Car (brand="Ford", model="Endavour", year=2012)
+
+Ford.start()

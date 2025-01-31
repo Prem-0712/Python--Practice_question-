66423f95c1fa0ae9fc906e11768b34cb8db76287
spdiey07 <premgangurde0712@gmail.com>
2025-01-31 10:06:38 +0530

    Update tables_using_while_loop.py

--- tables_using_while_loop.py
+++ tables_using_while_loop.py
@@ -1,21 +1,33 @@
-a = int(input("Enter your number: "))
-i = 1
+# ---------------TABLE USING WHILE LOOP---------
 
-while (i <= 10):
-    print(a, "*", i, "=", a * i)
-    i +=1
+# a = int(input("Enter your number: "))
+# i = 1
+# while ( i <= 10 ):
+#     print( a, "X", i, "=", (a * i) )
+#     i += 1
 
-a = int(input("Enter your number: "))
-j = 1
+# -------------------------TABLE USING FOR LOOP-------------------------
 
-while (j <= a):
-    i = 1
-    while(i <= 10):
-        print(j, "*", i, "=", j * i)
-        i += 1
-    print()
-    print("End of table")
-    print()
-    j += 1
-print()
-print("End of loop")
+# a = int(input("Enter your number: "))
+# print()
+# i = 10
+
+# for j in range(i):
+#     print ( a, "X", (j + 1), "=", (a * (j + 1)))
+
+
+
+# a = int(input("Enter your number: "))
+# j = 1
+
+# while (j <= a):
+#     i = 1
+#     while(i <= 10):
+#         print(j, "*", i, "=", j * i)
+#         i += 1
+#     print()
+#     print("End of table")
+#     print()
+#     j += 1
+# print()
+# print("End of loop")

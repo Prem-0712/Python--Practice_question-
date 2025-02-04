8231364f2582fb897f33ad30f0c0c9e23c093527
spdiey07 <premgangurde0712@gmail.com>
2025-02-04 18:17:24 +0530

    working

---
+++ ABSTRACTION/function.py
@@ -0,0 +1,13 @@
+from abstract import Shape
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return ( (self.side) ** 2)
+    
+    def perimeter(self):
+        return ( (self.side) * 4)
+    
+    
---
+++ ABSTRACTION/user.py
@@ -0,0 +1,6 @@
+from function import Square
+
+Chauras = Square(5)
+
+print(f"The area of Square is {Chauras.area()}")
+print(f"The permeter of Square is {Chauras.perimeter()}")
